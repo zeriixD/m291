21ded85679eadd555e0921aa68558d236d6138c8
KevinLoCurto <118447847+KevinLoCurto@users.noreply.github.com>
2024-10-23 08:22:53 +0200

    Start

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+      <h1>sgsgs</h1>
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style></style>
+
+<script>
+  const app = createApp({
+    setup() {
+      
+    }
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</script>
